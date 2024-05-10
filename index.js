0273768c59883feeff1ab78be1f6b92e35d8b974
pgthalita <thalita.pereira25@gmail.com>
2024-05-09 23:37:33 -0300

    projeto atualizado e finalizado

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 let nomeHeroi = "Atena"
-let xpDoHeroi = 15070
+let pontos = 15070
 let nivel 
 
-if (xpDoHeroi <= 1000){
+if (pontos <= 1000){
     nivel = "Ferro"
 }
-else if (xpDoHeroi >= 1001 && xpDoHeroi < 2000){
+else if (pontos >= 1001 && pontos < 2000){
     nivel = "Bronze"
 }
-else if (xpDoHeroi >= 2001 && xpDoHeroi < 5000){
+else if (pontos >= 2001 && pontos < 5000){
     nivel = "Prata"
 }
-else if (xpDoHeroi >= 5001 && xpDoHeroi < 7000){
+else if (pontos >= 5001 && pontos < 7000){
     nivel = "Ouro"
 }
-else if (xpDoHeroi >= 7001 && xpDoHeroi < 8000){
+else if (pontos >= 7001 && pontos < 8000){
     nivel = "Platina"
 }
-else if (xpDoHeroi >= 8001 && xpDoHeroi < 9000){
+else if (pontos >= 8001 && pontos < 9000){
     nivel = "Ascendente"
 }
-else if (xpDoHeroi >= 9001 && xpDoHeroi < 1000){
+else if (pontos >= 9001 && pontos < 1000){
     nivel = "Imortal"
 }
 else {
